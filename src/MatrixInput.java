1ae65f26f7b8a9b31be37e864db36934abcc8652
psyew2 <psyew2@nottingham.ac.uk>
2022-05-26 14:52:36 +0100

    Gui matrix dimension imput complete

--- src/MatrixInput.java
+++ src/MatrixInput.java
@@ -10,8 +10,9 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class MatrixInput {
+	Matrix matrix = new Matrix();
 	private JTextField[] dimensions = new JTextField[2];
-	private JTextField[][] matrix;
+	private JTextField[][] matrixIn;
 	private JTextField[][] enter;
 	private JLabel[] labels = new JLabel[2];
 	private JFrame contentFrame = new JFrame("Matrix Input");
@@ -38,12 +39,16 @@ public class MatrixInput {
 		}
 	}
 	public void createMatrixIn(int row, int col) {
-		matrix = new JTextField[row][col];
+		matrixGUI.setLayout(new GridLayout(row,col));
+		matrix.setSize(row, col);
+		matrixIn = new JTextField[row][col];
 		for(int i = 0; i < row; i++) {
             for(int j = 0; j < col; j++) {
-            	matrix[i][j] = new JTextField();
+            	matrixIn[i][j] = new JTextField();
+            	matrixGUI.add(matrixIn[i][j]);
             }
         }
+		contentFrame.add(matrixGUI, BorderLayout.CENTER);
 	}
 	public void createGUI() {
 		createDimInput();
@@ -54,8 +59,16 @@ public class MatrixInput {
 		submitDim.addActionListener(
 			new ActionListener() {
 				public void actionPerformed(ActionEvent event) {
+					int[] dim = new int[2];
+					for(int i = 0; i < 2; i++) {
+						dim[i] = Integer.parseInt(dimensions[i].getText());
+					}
+					
 					contentFrame.remove(dimGUI);
 					contentFrame.remove(submitDim);
+					System.out.println(dim[1]);
+					createMatrixIn(dim[0],dim[1]);
+					contentFrame.add(submitMat, BorderLayout.SOUTH);
 					contentFrame.revalidate();
 	                contentFrame.repaint();
 				}
